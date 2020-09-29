608ac7ea17b14ba9d560e66b8c07c8e368afa597
Konrad Smuga <smuggk@gmail.com>
2020-09-29 21:35:47 +0200

    fill else if options 2 and 3

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
-let playerMove ='papier';
-let computerMove = 'kamień';
+// let playerMove ='papier';
+// let computerMove = 'kamień';
 
-function printMessage(msg){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
+// function clearMessages(){
+// 	document.getElementById('messages').innerHTML = '';
+// }
+
+// printMessage('Zagralem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ' wygrywasz!');
+
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}else if( playerInput =='2'){
+	playerMove= 'papier';
+}else if(playerInput== '3'){
+	playerMove='nożyce';
 }
 
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
 
-printMessage('Zagralem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ' wygrywasz!');
+printMessage('Twój ruch to: ' + playerMove);
