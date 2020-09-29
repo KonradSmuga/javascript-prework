3e2fcd359b07e40d45fde45e6b8e1842245eef7d
Konrad Smuga <smuggk@gmail.com>
2020-09-29 21:43:32 +0200

    player decision in else if

--- js/script.js
+++ js/script.js
@@ -8,18 +8,35 @@
 // printMessage('Zagralem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ' wygrywasz!');
 
 
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-console.log('Gracz wpisał: ' + playerInput);
 
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+let computerMove = 'nieznany ruch';
 let playerMove = 'nieznany ruch';
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}else if(randomNumber == 2){
+	computerMove = 'papier';
+}else if(randomNumber == 3){
+	computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
+
+ let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+ console.log('Gracz wpisał: ' + playerInput);
 
 if(playerInput == '1'){
   playerMove = 'kamień';
-}else if( playerInput =='2'){
+ }else if( playerInput =='2'){
 	playerMove= 'papier';
-}else if(playerInput== '3'){
-	playerMove='nożyce';
-}
+ }else if(playerInput== '3'){
+ 	playerMove='nożyce';
+ }
 
 
 printMessage('Twój ruch to: ' + playerMove);
+
+
