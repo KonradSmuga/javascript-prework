5efa2a357a5a0cfadc9a9b966397aa21414fe337
Konrad Smuga <smuggk@gmail.com>
2020-10-02 16:29:51 +0200

    adding buttons to game

--- js/script.js
+++ js/script.js
@@ -7,63 +7,87 @@
 
 // printMessage('Zagralem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ' wygrywasz!');
 
-let randomNumber = Math.floor(Math.random() * 3 + 1);
-let computerMove = getMoveName(randomNumber);
-console.log('Komputer wylosowal: ' + randomNumber);
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-let playerMove = getMoveName(playerInput);
 
 
-console.log('Gracz wpisał: ' + playerInput);
-let score = displayResult(computerMove, playerMove);
 
-// if(randomNumber == 1){
-//   computerMove = 'kamień';
-// }else if(randomNumber == 2){
-// 	computerMove = 'papier';
-// }else if(randomNumber == 3){
-// 	computerMove = 'nożyce';
-// }
+function playGame(playerInput) {
+	clearMessages();
+	let randomNumber = Math.floor(Math.random() * 3 + 1);
+	let computerMove = getMoveName(randomNumber);
+	console.log('Komputer wylosowal: ' + randomNumber);
+	// let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+	let playerMove = getMoveName(playerInput);
 
-function getMoveName(argMoveId){
-	if(argMoveId == 1){
-	  return 'kamień';
-	} 
-	if(argMoveId ==2 ){
-	  return 'papier';
+
+	console.log('Gracz wpisał: ' + playerInput);
+	let score = displayResult(computerMove, playerMove);
+
+	// if(randomNumber == 1){
+	//   computerMove = 'kamień';
+	// }else if(randomNumber == 2){
+	// 	computerMove = 'papier';
+	// }else if(randomNumber == 3){
+	// 	computerMove = 'nożyce';
+	// }
+
+	function getMoveName(argMoveId) {
+		if (argMoveId == 1) {
+			return 'kamień';
+		}
+		if (argMoveId == 2) {
+			return 'papier';
+		}
+		if (argMoveId == 3) {
+			return 'nożyce';
+		}
 	}
-	if(argMoveId == 3 ){
-		return 'nożyce';
+
+
+	if (playerInput == "") {
+		playerMove = 'brak dodanego elementu'
+		score = 'nie podales elementu wiec nie mozna okreslic wyniku'
+		printMessage('nic nie dodales, wybierz opcje');
 	}
+
+
+	// if(playerInput == '1'){
+	//   playerMove = 'kamień';
+	//  }else if( playerInput =='2'){
+	// 	playerMove= 'papier';
+	//  }else if(playerInput== '3'){
+	//  	playerMove='nożyce';
+	//  }
+
+	function displayResult(argComputerMove, argPlayerMove) {
+		if (argComputerMove == 'kamień' && argPlayerMove == 'papier' || argComputerMove == 'nożyce' && argPlayerMove == 'kamień' || argComputerMove == 'papier' && argPlayerMove == 'nożyce') {
+			return 'Ty wygrywasz';
+		}
+		if (argComputerMove == argPlayerMove) {
+			return 'remis';
+		}
+		return 'wygrywa komputer';
+	}
+
+
+	printMessage('Mój ruch to: ' + computerMove);
+	printMessage('Gracza ruch to: ' + playerMove);
+	printMessage('wynik gry: ' + score)
+	console.log('wynik: ' + score);
+
 }
 
+let paperButton = document.getElementById('play-paper')
+let rockButton = document.getElementById('play-rock')
+let scisorsButton = document.getElementById('play-scisors')
 
-if(playerInput == ""){
-	playerMove = 'brak dodanego elementu'
-	score = 'nie podales elementu wiec nie mozna okreslic wyniku'
-	printMessage('nic nie dodales, wybierz opcje');
-}
+paperButton.addEventListener('click', function(){
+	playGame(1)
+})
+rockButton.addEventListener('click', function(){
+	playGame(2)
+})
 
+scisorsButton.addEventListener('click', function(){
+	playGame(3)
+})
 
-// if(playerInput == '1'){
-//   playerMove = 'kamień';
-//  }else if( playerInput =='2'){
-// 	playerMove= 'papier';
-//  }else if(playerInput== '3'){
-//  	playerMove='nożyce';
-//  }
-
-function displayResult (argComputerMove, argPlayerMove){
-	if(argComputerMove == 'kamień' && argPlayerMove =='papier' || argComputerMove == 'nożyce' && argPlayerMove == 'kamień' || argComputerMove == 'papier' && argPlayerMove =='nożyce'){
-	return	'Ty wygrywasz';
-	}
-	if(argComputerMove == argPlayerMove){ 
-	return  'remis';
-	}
-	return	'wygrywa komputer';
-}
-
-printMessage('Mój ruch to: ' + computerMove);
-printMessage('Gracza ruch to: ' +playerMove);
-printMessage ('wynik gry: ' +score)
-console.log('wynik: ' + score);
